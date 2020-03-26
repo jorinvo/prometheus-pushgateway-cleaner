c4e14353b85d4b71cbe794e57e14b7595839566e
Jorin Vogel <vogel@heartbeat-med.de>
2020-03-26 16:37:31 +0100

    Do not cleanup its own success metric

--- test/prometheus_pushgateway_cleaner_test.clj
+++ test/prometheus_pushgateway_cleaner_test.clj
@@ -4,39 +4,64 @@
             [prometheus-pushgateway-cleaner :as p])
   (:import (java.net URI)))
 
+(defn metric-response []
+  {:headers {"server" "nginx/1.17.4"
+             "date" "Wed, 26 Feb 2020 10:11:56 GMT"
+             "content-type" "text/plain; version=0.0.4; charset=utf-8"
+             "transfer-encoding" "chunked"
+             "content-encoding" "gzip"}
+   :status 200
+   :body (slurp "test/test-metrics.prom")})
+
+(defn mock-req [reqs]
+  (fn [req]
+    (if (= "https://example.com/metrics/" (str (:url req)))
+     (metric-response)
+     (swap! reqs (fn [r] (conj r req))))))
+
+(defn mock-now []
+  (+ (* 1.5827112617848473E9 1000) (p/min->ms 60)))
+
+(def common-args
+    {:log (fn [_] nil)
+     :metric-url (URI. "https://example.com/metrics/")
+     :job-url (URI. "https://example.com/metrics/job/")
+     :expiration-in-minutes 60
+     :report-metrics false})
+
+(def common-response
+  [{:url (URI. "https://example.com/metrics/job/api/institute_id/number+two/")
+    :basic-auth nil
+    :method :delete}
+   {:url (URI. "https://example.com/metrics/job/api/institute_id/third-one/")
+    :basic-auth nil
+    :method :delete}])
+
 (deftest sends-correct-delete-requests
   (is
-    (= (let [metric-response {:headers {"server" "nginx/1.17.4"
-                                        "date" "Wed, 26 Feb 2020 10:11:56 GMT"
-                                        "content-type" "text/plain; version=0.0.4; charset=utf-8"
-                                        "transfer-encoding" "chunked"
-                                        "content-encoding" "gzip"}
-                              :status 200
-                              :body (slurp "test/test-metrics.prom")}
-             reqs (atom [])]
-         (with-redefs [p/now-in-ms (fn [] (+ (* 1.5827112617848473E9 1000) (p/min->ms 60)))
-                       http/request (fn [req]
-                                      (if (= "https://example.com/metrics/" (str (:url req)))
-                                        metric-response
-                                        (swap! reqs (fn [r] (conj r req)))))]
-           (p/run {:log (fn [_] nil)
-                   :metric-url (URI. "https://example.com/metrics/")
-                   :job-url (URI. "https://example.com/metrics/job/")
-                   :expiration-in-minutes 60
-                   :metric-name "metric"
-                   :report-metrics true
-                   :success-metric "metric"})
+    (= (let [reqs (atom [])]
+         (with-redefs [p/now-in-ms mock-now
+                       http/request (mock-req reqs)]
+           (p/run common-args)
            @reqs))
-       [{:url (URI. "https://example.com/metrics/job/api/institute_id/number+two/")
-         :basic-auth nil
-         :method :delete}
-        {:url (URI. "https://example.com/metrics/job/api/institute_id/third-one/")
-         :basic-auth nil
-         :method :delete}
-        {:url (URI. "https://example.com/metrics/job/metric")
-         :basic-auth nil
-         :method :put
-         :body "metric 1582714861\n"}
-        ])))
+       (conj common-response
+             {:url (URI. "https://example.com/metrics/job/success/")
+              :basic-auth nil
+              :method :delete}))))
 
+(deftest report-metrics
+  (is
+    (= (let [reqs (atom [])]
+         (with-redefs [p/now-in-ms mock-now
+                       http/request (mock-req reqs)]
+           (p/run (assoc common-args
+                         :report-metrics true
+                         :success-metric "success"))
+           @reqs))
+       (conj common-response
+             {:url (URI. "https://example.com/metrics/job/success")
+              :basic-auth nil
+              :method :put
+              :body "success 1582714861\n"}
+             ))))
 
